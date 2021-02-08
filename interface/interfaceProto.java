0d48ca347ee53d689dfc158ad7b639c70d882551
Loïc GHEKIERE <loic.ghekiere@hotmail.com>
2021-02-08 08:10:23 +0100

    création prototype interface

---
+++ interface/interfaceProto.java
@@ -0,0 +1,11 @@
+package interface;
+import javax.swing.*;
+
+public class interfaceProto {
+
+    public static void main(String args * ){
+        JFrame fen = new JFrame("Hello World !");
+        fen.setVisible(true);
+    }
+
+}
